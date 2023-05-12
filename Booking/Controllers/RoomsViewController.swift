84a77d6145301ba9a22c55cbcd16ca48fa215eb2
TillieFang <cindy19971116@gmail.com>
2023-05-12 15:19:22 +1000

    Changes on layout and table view on the select a room page

--- Booking/Controllers/RoomsViewController.swift
+++ Booking/Controllers/RoomsViewController.swift
@@ -7,25 +7,43 @@
 
 import UIKit
 
+struct roomInfo {
+    var roomNumber : String
+    var capacity : Int
+}
+
 class RoomsViewController: UIViewController {
 
     @IBOutlet weak var timeDurationTextField: UITextField!
-    
     @IBOutlet weak var timeDurationStepper: UIStepper!
+    @IBOutlet weak var roomsTableView: UITableView!
+    @IBOutlet weak var startingTimeDatePicker: UIDatePicker!
     
+    var roomsTableData : [roomInfo] = []
     
     var timeDuration = 0
+    var selectedIndex : IndexPath?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        
+        roomsTableData = [
+            roomInfo(roomNumber: "room1", capacity: 1),
+            roomInfo(roomNumber: "room2", capacity: 2),
+            roomInfo(roomNumber: "room3", capacity: 3)]
+        
+    
     }
+    
+    
     
     @IBAction func stepperTapped(_ sender: UIStepper) {
         let stepperValue = Int(sender.value)
         timeDurationTextField.text = formatTime(timeValue: stepperValue)
     }
+    
     
     func formatTime(timeValue:Int) -> String{
         let hour = Int(timeValue / 60)
@@ -37,9 +55,40 @@ class RoomsViewController: UIViewController {
             let formattedTime = String("\(hour) hr  \(minute) min")
             return formattedTime
         }
-        
+    }
+}
+
+
+
+
+extension RoomsViewController : UITableViewDelegate,UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        if selectedIndex == indexPath {
+//            return 200
+//        }
+//        return 60
+//    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return roomsTableData.count
     }
     
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let roomCell = tableView.dequeueReusableCell(withIdentifier: "roomCell", for: indexPath)
+        let room = roomsTableData[indexPath.row]
+        roomCell.textLabel?.text = room.roomNumber
+        roomCell.detailTextLabel?.text = "capacity: \(room.capacity)"
+        return roomCell
+    }
+    
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        selectedIndex = indexPath
+//
+//        tableView.beginUpdates()
+//        tableView.reloadRows(at: [selectedIndex!], with: .none)
+//        tableView.endUpdates()
+//    }
 }
