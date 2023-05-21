2b91496d027457e0ea3b82183d3dc9a112a620f0
TillieFang <cindy19971116@gmail.com>
2023-05-21 23:29:37 +1000

    Create interface of manage bookings/Adjust layout of confirmation page

---
+++ Booking/Controllers/ManageBookingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ManageBookingViewController.swift
+//  Booking
+//
+//  Created by Ting on 2023/5/21.
+//
+
+import UIKit
+
+class ManageBookingViewController : UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
